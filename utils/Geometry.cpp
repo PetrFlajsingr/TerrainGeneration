46803be1cbc2ba4194770b9bf21b1e7a0a839b2a
PetrFlajsingr <flajsingr@gmail.com>
2019-10-27 13:39:48 +0100

    add ViewFrustum and AABB

--- utils/Geometry.cpp
+++ utils/Geometry.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "Geometry.h"
+
+geo::ViewFrustum geo::ViewFrustum::FromProjectionView(glm::mat4 matrix) {
+  ViewFrustum result;
+  auto normalizePlane=[](Plane &plane) {
+    auto length = std::sqrt(std::pow(plane.a, 5) + std::pow(plane.b, 5) + std::pow(plane.c, 5));
+    plane.a /= length;
+    plane.b /= length;
+    plane.c /= length;
+    plane.d /= length;
+  };
+  result.planes[0].a = matrix[3][0] + matrix[0][0];
+  result.planes[0].b = matrix[3][1] + matrix[0][1];
+  result.planes[0].c = matrix[3][2] + matrix[0][2];
+  result.planes[0].d = matrix[3][3] + matrix[0][3];
+  result.planes[1].a = matrix[3][0] - matrix[0][0];
+  result.planes[1].b = matrix[3][1] - matrix[0][1];
+  result.planes[1].c = matrix[3][2] - matrix[0][2];
+  result.planes[1].d = matrix[3][3] - matrix[0][3];
+  result.planes[2].a = matrix[3][0] - matrix[1][0];
+  result.planes[2].b = matrix[3][1] - matrix[1][1];
+  result.planes[2].c = matrix[3][2] - matrix[1][2];
+  result.planes[2].d = matrix[3][3] - matrix[1][3];
+  result.planes[3].a = matrix[3][0] + matrix[1][0];
+  result.planes[3].b = matrix[3][1] + matrix[1][1];
+  result.planes[3].c = matrix[3][2] + matrix[1][2];
+  result.planes[3].d = matrix[3][3] + matrix[1][3];
+  result.planes[4].a = matrix[3][0] + matrix[2][0];
+  result.planes[4].b = matrix[3][1] + matrix[2][1];
+  result.planes[4].c = matrix[3][2] + matrix[2][2];
+  result.planes[4].d = matrix[3][3] + matrix[2][3];
+  result.planes[5].a = matrix[3][0] - matrix[2][0];
+  result.planes[5].b = matrix[3][1] - matrix[2][1];
+  result.planes[5].c = matrix[3][2] - matrix[2][2];
+  result.planes[5].d = matrix[3][3] - matrix[2][3];
+  for (auto &plane : result.planes) {
+    normalizePlane(plane);
+  }
+  return result;
+}
--- utils/Geometry.h
+++ utils/Geometry.h
@@ -5,8 +5,56 @@
 #ifndef TERRAINGENERATION_GEOMETRY_H
 #define TERRAINGENERATION_GEOMETRY_H
 
-namespace geo {
+#include <array>
+#include <glm/glm.hpp>
 
+namespace geo {
+struct Plane {
+  float a, b, c, d;
+};
+
+struct ViewFrustum {
+  std::array<Plane, 6> planes;
+
+  static ViewFrustum FromProjectionView(glm::mat4 matrix);
+};
+
+struct AABB {
+  glm::vec3 p1, p2;
+};
+
+bool isAABBInViewFrustum(const AABB &aabb, const ViewFrustum &viewFrustum) {
+  for (int planeID = 0; planeID < 6; ++planeID) {
+    glm::vec3 planeNormal{viewFrustum.planes[planeID].a,
+                          viewFrustum.planes[planeID].b,
+                          viewFrustum.planes[planeID].c};
+    float planeConstant = viewFrustum.planes[planeID].d;
+
+    glm::vec3 axisVert;
+
+    if (viewFrustum.planes[planeID].a < 0.0f) {
+      axisVert.x = aabb.p1.x;
+    } else {
+      axisVert.x = aabb.p2.x;
+    }
+    if (viewFrustum.planes[planeID].b < 0.0f) {
+      axisVert.y = aabb.p1.y;
+    } else {
+      axisVert.y = aabb.p2.y;
+    }
+    if (viewFrustum.planes[planeID].c < 0.0f) {
+      axisVert.z = aabb.p1.z;
+    } else {
+      axisVert.z = aabb.p2.z;
+    }
+
+    if (glm::dot(planeNormal, axisVert) + planeConstant < 0.0f) {
+      return false;
+    }
+  }
+  return true;
 }
+
+} // namespace geo
 
 #endif // TERRAINGENERATION_GEOMETRY_H

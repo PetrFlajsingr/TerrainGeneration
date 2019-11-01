67721b2ba1da8b8c868da637b948cd40e0785438
PetrFlajsingr <flajsingr@gmail.com>
2019-11-01 09:11:21 +0100

    add meta constructs for glm::vec, iterable

--- utils/ShaderLiterals.h
+++ utils/ShaderLiterals.h
@@ -11,31 +11,38 @@
 namespace ShaderLiterals {
 using ShaderPtr = std::shared_ptr<ge::gl::Shader>;
 
-inline ShaderPtr operator""_vert(const char* name, std::size_t length) {
-    return std::make_shared<ge::gl::Shader>(GL_VERTEX_SHADER, loadShaderFile(name, ShaderType::Vertex));
+inline ShaderPtr operator""_vert(const char *name, std::size_t) {
+  return std::make_shared<ge::gl::Shader>(
+      GL_VERTEX_SHADER, loadShaderFile(name, ShaderType::Vertex));
 }
 
-inline ShaderPtr operator""_frag(const char* name, std::size_t length) {
-  return std::make_shared<ge::gl::Shader>(GL_FRAGMENT_SHADER, loadShaderFile(name, ShaderType::Fragment));
+inline ShaderPtr operator""_frag(const char *name, std::size_t) {
+  return std::make_shared<ge::gl::Shader>(
+      GL_FRAGMENT_SHADER, loadShaderFile(name, ShaderType::Fragment));
 }
 
-inline ShaderPtr operator""_geom(const char* name, std::size_t length) {
-  return std::make_shared<ge::gl::Shader>(GL_GEOMETRY_SHADER, loadShaderFile(name, ShaderType::Geometry));
+inline ShaderPtr operator""_geom(const char *name, std::size_t) {
+  return std::make_shared<ge::gl::Shader>(
+      GL_GEOMETRY_SHADER, loadShaderFile(name, ShaderType::Geometry));
 }
 
-inline ShaderPtr operator""_comp(const char* name, std::size_t length) {
-  return std::make_shared<ge::gl::Shader>(GL_COMPUTE_SHADER, loadShaderFile(name, ShaderType::Compute));
+inline ShaderPtr operator""_comp(const char *name, std::size_t) {
+  return std::make_shared<ge::gl::Shader>(
+      GL_COMPUTE_SHADER, loadShaderFile(name, ShaderType::Compute));
 }
 
-inline ShaderPtr operator""_tesc(const char* name, std::size_t length) {
-  return std::make_shared<ge::gl::Shader>(GL_TESS_CONTROL_SHADER, loadShaderFile(name, ShaderType::TesselationControl));
+inline ShaderPtr operator""_tesc(const char *name, std::size_t) {
+  return std::make_shared<ge::gl::Shader>(
+      GL_TESS_CONTROL_SHADER,
+      loadShaderFile(name, ShaderType::TesselationControl));
 }
 
-inline ShaderPtr operator""_tese(const char* name, std::size_t length) {
-  return std::make_shared<ge::gl::Shader>(GL_TESS_EVALUATION_SHADER, loadShaderFile(name, ShaderType::TesselationEvaluation));
+inline ShaderPtr operator""_tese(const char *name, std::size_t) {
+  return std::make_shared<ge::gl::Shader>(
+      GL_TESS_EVALUATION_SHADER,
+      loadShaderFile(name, ShaderType::TesselationEvaluation));
 }
 
-
-}
+} // namespace ShaderLiterals
 
 #endif // TERRAINGENERATION_SHADERLITERALS_H
--- utils/meta.h
+++ utils/meta.h
@@ -5,8 +5,8 @@
 #ifndef TERRAINGENERATION_META_H
 #define TERRAINGENERATION_META_H
 
-#include <type_traits>
 #include <chrono>
+#include <type_traits>
 
 template <class T, template <class...> class Template>
 struct is_specialization : std::false_type {};
@@ -15,12 +15,36 @@ template <template <class...> class Template, class... Args>
 struct is_specialization<Template<Args...>, Template> : std::true_type {};
 
 template <typename T, template <class...> class U>
-static inline constexpr bool is_specialization_v = is_specialization<T, U>::value;
+static inline constexpr bool is_specialization_v =
+    is_specialization<T, U>::value;
 
 template <typename T>
-static inline constexpr bool is_duration_v = is_specialization<T, std::chrono::duration>::value;
+static inline constexpr bool is_duration_v =
+    is_specialization<T, std::chrono::duration>::value;
 
 template <typename T, typename... Types>
-static inline constexpr bool is_one_of_v = (std::is_same_v<T, Types> || ... || false);
+static inline constexpr bool is_one_of_v = (std::is_same_v<T, Types> || ... ||
+                                            false);
+
+template <typename T, typename = void> struct is_iterable : std::false_type {};
+template <typename T>
+struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())),
+                                  decltype(std::end(std::declval<T>()))>>
+    : std::true_type {};
+
+template <typename T> constexpr bool is_iterable_v = is_iterable<T>::value;
+
+template <class T, template <class, glm::precision> class Template>
+struct is_vec_specialization : std::false_type {};
+template <template <class, glm::precision> class Template, typename ValueType,
+          glm::precision P>
+struct is_vec_specialization<Template<ValueType, P>, Template>
+    : std::true_type {};
+
+template <typename T>
+constexpr bool is_vec_specialisation_v =
+    is_vec_specialization<std::decay_t<T>, glm::tvec4>::value ||
+    is_vec_specialization<std::decay_t<T>, glm::tvec3>::value ||
+    is_vec_specialization<std::decay_t<T>, glm::tvec2>::value;
 
 #endif // TERRAINGENERATION_META_H

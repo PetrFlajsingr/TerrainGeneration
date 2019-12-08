9ffc0a306ed8ab35006497dcfa9423be6d253508
PetrFlajsingr <flajsingr@gmail.com>
2019-12-08 17:16:09 +0100

    prepare Textures for cascades and activation functions for render passes

--- rendering/shading/CascadedShadowMap.cpp
+++ rendering/shading/CascadedShadowMap.cpp
@@ -3,3 +3,40 @@
 //
 
 #include "CascadedShadowMap.h"
+#include "utils/types/Range.h"
+#include <geGL/StaticCalls.h>
+
+using namespace MakeRange;
+
+CascadedShadowMap::CascadedShadowMap(unsigned int cascadeCount,
+                                     unsigned int textureWidth,
+                                     unsigned int textureHeight)
+    : cascadeCount(cascadeCount), width(textureWidth), height(textureHeight) {
+  lightProjections.resize(cascadeCount);
+  glm::vec4 borderColor{1.0, 1.0, 1.0, 1.0};
+  for ([[maybe_unused]] auto _ : range(cascadeCount)) {
+    depthMaps.emplace_back(GL_TEXTURE_2D, GL_DEPTH_COMPONENT, 0, width, height);
+    ge::gl::glTextureImage2DEXT(depthMaps.back().getId(), GL_TEXTURE_2D, 0,
+                                GL_DEPTH_COMPONENT, width, height, 0,
+                                GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
+    depthMaps.back().texParameteri(GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    depthMaps.back().texParameteri(GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    depthMaps.back().texParameteri(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    depthMaps.back().texParameteri(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+    depthMaps.back().texParameterfv(GL_TEXTURE_BORDER_COLOR, &borderColor[0]);
+  }
+
+  ge::gl::glFramebufferDrawBufferEXT(depthMapFBO.getId(), GL_NONE);
+  ge::gl::glFramebufferReadBufferEXT(depthMapFBO.getId(), GL_NONE);
+}
+
+void CascadedShadowMap::bindCascade(unsigned int index) {
+  depthMapFBO.attachTexture(GL_DEPTH_ATTACHMENT, &depthMaps[index]);
+}
+
+void CascadedShadowMap::bindRender() {
+  for (auto i : range(depthMaps.size())) {
+    depthMaps[i].bind(shadowTextureUnitOffset + i);
+  }
+}
--- rendering/shading/CascadedShadowMap.h
+++ rendering/shading/CascadedShadowMap.h
@@ -4,9 +4,38 @@
 
 #ifndef TERRAINGENERATION_CASCADEDSHADOWMAP_H
 #define TERRAINGENERATION_CASCADEDSHADOWMAP_H
-
+#include <glm/glm.hpp>
+#include <geGL/geGL.h>
+#include <geGL_utils.h>
+using namespace ShaderLiterals;
 class CascadedShadowMap {
+public:
+  CascadedShadowMap(unsigned int cascadeCount, unsigned int textureWidth, unsigned int textureHeight);
 
+  void bindCascade(unsigned int index);
+
+  void bindRender();
+
+private:
+  static constexpr unsigned int shadowTextureUnitOffset = 0;
+  std::vector<glm::mat4> lightProjections;
+
+  glm::mat4 lightSpaceMatrix;
+  glm::vec3 lightPos;
+  glm::vec3 target{0.0f, 0.0f, 0.0f};
+  glm::vec3 up{0.0f, 1.0f, 0.0f};
+  GLint m_viewport[4]{};
+
+  unsigned int width = 4096;
+  unsigned int height = 4096;
+  unsigned int cascadeCount;
+
+  std::vector<ge::gl::Texture> depthMaps;
+
+  ge::gl::Framebuffer depthMapFBO;
+
+  std::shared_ptr<ge::gl::Program> program = std::make_shared<ge::gl::Program>(
+      "shadow_map/sm"_vert, "shadow_map/sm"_frag);
 };
 
 #endif // TERRAINGENERATION_CASCADEDSHADOWMAP_H

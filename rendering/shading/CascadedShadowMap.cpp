6a7d1239dd594043a01ded76ea236c4fdaf26a2a
PetrFlajsingr <flajsingr@gmail.com>
2019-12-08 18:55:43 +0100

    implement cascade shadow map texture rendering

--- rendering/shading/CascadedShadowMap.cpp
+++ rendering/shading/CascadedShadowMap.cpp
@@ -5,6 +5,7 @@
 #include "CascadedShadowMap.h"
 #include "utils/types/Range.h"
 #include <geGL/StaticCalls.h>
+#include <glm/gtc/matrix_transform.hpp>
 
 using namespace MakeRange;
 
@@ -13,6 +14,7 @@ CascadedShadowMap::CascadedShadowMap(unsigned int cascadeCount,
                                      unsigned int textureHeight)
     : cascadeCount(cascadeCount), width(textureWidth), height(textureHeight) {
   lightProjections.resize(cascadeCount);
+  perspective.resize(cascadeCount);
   glm::vec4 borderColor{1.0, 1.0, 1.0, 1.0};
   for ([[maybe_unused]] auto _ : range(cascadeCount)) {
     depthMaps.emplace_back(GL_TEXTURE_2D, GL_DEPTH_COMPONENT, 0, width, height);
@@ -39,4 +41,72 @@ void CascadedShadowMap::bindRender() {
   for (auto i : range(depthMaps.size())) {
     depthMaps[i].bind(shadowTextureUnitOffset + i);
   }
+}
+const glm::vec3 &CascadedShadowMap::getLightPos() const { return lightPos; }
+void CascadedShadowMap::setLightPos(const glm::vec3 &lightPos) {
+  CascadedShadowMap::lightPos = lightPos;
+}
+const glm::vec3 &CascadedShadowMap::getTarget() const { return target; }
+void CascadedShadowMap::setTarget(const glm::vec3 &target) {
+  CascadedShadowMap::target = target;
+}
+
+glm::mat4 CascadedShadowMap::calculateOrthoMatrices(const glm::mat4 &cameraView,
+                                                    float cameraNear,
+                                                    float cameraFar,
+                                                    float aspectRatio,
+                                                    float fieldOfView) {
+  std::vector<float> cascadeBoundaries;
+  cascadeBoundaries.resize(cascadeCount + 1);
+  const auto max = static_cast<float>(cascadeCount + 1);
+  for (auto i : range(cascadeCount)) {
+    cascadeBoundaries[i] = cameraNear + i / max * (cameraFar - cameraNear);
+  }
+
+  const auto invertedCameraView = glm::inverse(cameraView);
+  const auto lightView = glm::lookAt(lightPos, target, up);
+  const auto tanHalfHorizontalFOV = glm::tan(glm::radians(fieldOfView / 2.0f));
+  const auto tanHalfVerticalFOV =
+      glm::tan(glm::radians((fieldOfView * aspectRatio / 2.0f)));
+
+  for (auto i : range(cascadeCount)) {
+    const auto xn = cascadeBoundaries[i] * tanHalfHorizontalFOV;
+    const auto xf = cascadeBoundaries[i + 1] * tanHalfHorizontalFOV;
+    const auto yn = cascadeBoundaries[i] * tanHalfVerticalFOV;
+    const auto yf = cascadeBoundaries[i + 1] * tanHalfVerticalFOV;
+
+    auto frustumCorners =
+        std::vector{glm::vec4{xn, yn, cascadeBoundaries[i], 1.0},
+                    {-xn, yn, cascadeBoundaries[i], 1.0},
+                    {xn, -yn, cascadeBoundaries[i], 1.0},
+                    {-xn, -yn, cascadeBoundaries[i], 1.0},
+
+                    {xf, yf, cascadeBoundaries[i + 1], 1.0},
+                    {-xf, yf, cascadeBoundaries[i + 1], 1.0},
+                    {xf, -yf, cascadeBoundaries[i + 1], 1.0},
+                    {-xf, -yf, cascadeBoundaries[i + 1], 1.0}};
+
+    float minX = std::numeric_limits<float>::max();
+    float maxX = std::numeric_limits<float>::min();
+    float minY = std::numeric_limits<float>::max();
+    float maxY = std::numeric_limits<float>::min();
+    float minZ = std::numeric_limits<float>::max();
+    float maxZ = std::numeric_limits<float>::min();
+
+    for (auto &corner : frustumCorners) {
+      const glm::vec4 vW = invertedCameraView * corner;
+      corner = lightView * vW;
+
+      minX = glm::min(minX, corner.x);
+      maxX = glm::max(maxX, corner.x);
+      minY = glm::min(minY, corner.y);
+      maxY = glm::max(maxY, corner.y);
+      minZ = glm::min(minZ, corner.z);
+      maxZ = glm::max(maxZ, corner.z);
+    }
+    perspective[i] = glm::ortho(minX, maxX, minY, maxY, minZ, maxZ);
+  }
+}
+glm::mat4 CascadedShadowMap::lightSpaceMatrix(unsigned int cascade) const {
+  return perspective[cascade] * glm::lookAt(lightPos, target, up);
 }
--- rendering/shading/CascadedShadowMap.h
+++ rendering/shading/CascadedShadowMap.h
@@ -4,27 +4,40 @@
 
 #ifndef TERRAINGENERATION_CASCADEDSHADOWMAP_H
 #define TERRAINGENERATION_CASCADEDSHADOWMAP_H
-#include <glm/glm.hpp>
 #include <geGL/geGL.h>
 #include <geGL_utils.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <meta/meta.h>
+#include <utils/types/Range.h>
+
 using namespace ShaderLiterals;
 class CascadedShadowMap {
 public:
-  CascadedShadowMap(unsigned int cascadeCount, unsigned int textureWidth, unsigned int textureHeight);
-
-  void bindCascade(unsigned int index);
+  CascadedShadowMap(unsigned int cascadeCount, unsigned int textureWidth,
+                    unsigned int textureHeight);
 
   void bindRender();
+
+  [[nodiscard]] const glm::vec3 &getLightPos() const;
+  void setLightPos(const glm::vec3 &lightPos);
+  [[nodiscard]] const glm::vec3 &getTarget() const;
+  void setTarget(const glm::vec3 &target);
+
+  [[nodiscard]] glm::mat4 lightSpaceMatrix(unsigned int cascade) const;
+
+  template <SimpleInvocable F>
+  void renderShadowMap(F renderFunction, const glm::mat4 &cameraView,
+                       float cameraNear, float cameraFar, float aspectRatio,
+                       float fieldOfView);
 
 private:
   static constexpr unsigned int shadowTextureUnitOffset = 0;
   std::vector<glm::mat4> lightProjections;
-
-  glm::mat4 lightSpaceMatrix;
+  std::vector<glm::mat4> perspective;
   glm::vec3 lightPos;
   glm::vec3 target{0.0f, 0.0f, 0.0f};
   glm::vec3 up{0.0f, 1.0f, 0.0f};
-  GLint m_viewport[4]{};
 
   unsigned int width = 4096;
   unsigned int height = 4096;
@@ -34,8 +47,33 @@ private:
 
   ge::gl::Framebuffer depthMapFBO;
 
-  std::shared_ptr<ge::gl::Program> program = std::make_shared<ge::gl::Program>(
-      "shadow_map/sm"_vert, "shadow_map/sm"_frag);
+  ge::gl::Program program{"shadow_map/sm"_vert, "shadow_map/sm"_frag};
+
+  glm::mat4 calculateOrthoMatrices(const glm::mat4 &cameraView,
+                                   float cameraNear, float cameraFar,
+                                   float aspectRatio, float fieldOfView);
+
+  void bindCascade(unsigned int index);
 };
+
+template <SimpleInvocable F>
+void CascadedShadowMap::renderShadowMap(F renderFunction,
+                                        const glm::mat4 &cameraView,
+                                        float cameraNear, float cameraFar,
+                                        float aspectRatio, float fieldOfView) {
+  using namespace MakeRange;
+  calculateOrthoMatrices(cameraView, cameraNear, cameraFar, aspectRatio,
+                         fieldOfView);
+  program.use();
+  depthMapFBO.bind(GL_FRAMEBUFFER);
+  for (auto i : range(cascadeCount)) {
+    bindCascade(i);
+    const auto lightSpaceMatrix =
+        perspective[i] * glm::lookAt(lightPos, target, up);
+    program.setMatrix4fv("lightSpaceMatrix", &lightSpaceMatrix[0][0]);
+    renderFunction();
+  }
+  depthMapFBO.unbind(GL_FRAMEBUFFER);
+}
 
 #endif // TERRAINGENERATION_CASCADEDSHADOWMAP_H

6ec7f19310d085159a54e11145a5d1978e18048d
PetrFlajsingr <xflajs00@stud.fit.vutbr.cz>
2019-12-14 18:30:39 +0100

    refactor TimedEvent class

---
+++ ui/TimedEvent.cpp
@@ -0,0 +1,34 @@
+//
+// Created by petr on 12/14/19.
+//
+
+#include "TimedEvent.h"
+
+bool sdl2cpp::ui::TimedEvent::shouldFire(std::chrono::milliseconds time) {
+  if (!isValid()) {
+    return false;
+  }
+  bool result = time - start >= duration;
+  valid = !result || type == Type::Infinite || (type == Type::Repeated && (repetitions > fireCount));
+  return result && enabled;
+}
+
+bool sdl2cpp::ui::TimedEvent::isValid() { return valid; }
+
+void sdl2cpp::ui::TimedEvent::setEnabled(bool enabled) {
+  TimedEvent::enabled = enabled;
+}
+
+bool sdl2cpp::ui::TimedEvent::isEnabled() const {
+  return enabled;
+}
+
+void sdl2cpp::ui::TimedEvent::invalidate() {
+  valid = false;
+}
+
+void sdl2cpp::ui::TimedEvent::operator()(std::chrono::milliseconds now) {
+  start = now;
+  ++fireCount;
+  event();
+}

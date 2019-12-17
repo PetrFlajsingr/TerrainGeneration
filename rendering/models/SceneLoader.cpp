fc2d045914c49929e57796d8fe8d9ee441f1350a
Petr <xflajs00@stud.fit.vutbr.cz>
2019-12-17 14:04:16 +0100

    fix warning

--- rendering/models/SceneLoader.cpp
+++ rendering/models/SceneLoader.cpp
@@ -112,7 +112,7 @@ SceneLoader::iterator::operator=(const SceneLoader::iterator &other) {
   return *this;
 }
 
-bool SceneLoader::iterator::operator==(const SceneLoader::iterator &rhs) const {
+bool SceneLoader::iterator::operator==([[maybe_unused]] const SceneLoader::iterator &rhs) const {
   return !loader->isValid();
 }
 
